b2b0b23e53ff9c56e1d8a81cc43c8062afad7dab
Ardavel <wojciech.szalapski@gmail.com>
2015-04-16 17:04:45 +0200

    Comments translated to Polish.

--- pietnastka/solver/src/main/java/sise/pietnastka/solver/CollisionEvaluator.java
+++ pietnastka/solver/src/main/java/sise/pietnastka/solver/CollisionEvaluator.java
@@ -1,19 +1,20 @@
 package sise.pietnastka.solver;
 
 /**
- * Linear collision heuristic.
- * 
- * Each linear collision (two values swapped in one row) adds 2 to the result.
+ * Heurystyka liniowej kolizji.
+ *
+ * Każda liniowa kolizja (dwie wartości zamienione miejscami w jednym wierszu
+ * dodają 2 do wyniku heurystyki Manhattan.
  */
 public class CollisionEvaluator extends Evaluator {
 
     /**
-     * Rows.
+     * Liczba wierszy.
      */
     private int m;
 
     /**
-     * Columns.
+     * Liczba kolumn.
      */
     private int n;
 
@@ -28,30 +29,32 @@ public class CollisionEvaluator extends Evaluator {
         int manhattanHeuristic = super.evaluate(state);
 
         int linearCollisions = 0;
-        
-        // for each row
+
+        // poniżej rozpatrywany jest docelowy (rozwiązany) układ
+        // dla każdego wiersza
         for (int row = 0; row < m; ++row) {
-            // take all fields from this row apart from the last one
+            // weź wszystkie pola z tego wiersza poza ostatnim polem
             for (int f1 = row * n; f1 < (row + 1) * n - 1; ++f1) {
-                // for all fields which are in this row to the right from f1
+                // dla każdego pola f1 w tym wierszu weź wszystkie pola po prawej stronie
                 for (int f2 = f1 + 1; f2 < (row + 1) * n; ++f2) {
-                    // remember that the last value in last row is 0
+                    // należy pamiętać, że ostatnia wartość w ostatnim wierszu jest równa 0
                     if (f2 == m * n) {
                         f2 = 0;
                     }
-                    
+
+                    // pobierz faktyczne położenie dwóch rozpatrywanych wartości
                     int f1Column = positionsOfNumbers[f1][1];
                     int f2Column = positionsOfNumbers[f2][1];
-                    
-                    // check if f1 and f2 are swapped in current row
+
+                    // sprawdź, czy wartości f1 i f2 nie są w bieżącym wierszu zamienione miejscami
                     if ((f1 < f2) != (f1Column < f2Column)) {
                         ++linearCollisions;
                     }
                 }
             }
         }
-        
-        // each linear collision adds at least 2 moves to Manhattan heuristic
+
+        // każda liniowa kolizja dodaje co najmniej 2 ruchy do wyniku danego przez heurystykę Manhattan
         return manhattanHeuristic + 2 * linearCollisions;
     }
 }
